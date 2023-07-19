55a09e53cbdaae95671c1af5e4c39c01072c934e
Stephanos Massouras <s.massouras@outlook.com>
2023-07-19 16:41:35 +0300

    brings groups and qeustions from selected template

--- src/pages/admin/ViewProjects.vue
+++ src/pages/admin/ViewProjects.vue
@@ -19,13 +19,17 @@
       :rules="[(val) => (val && val.length > 0) || 'Please type something']"
       class="q-ma-md q-mb-ml"
     />
-    <q-select
-      filled
-      v-model="selectedTemplate"
-      :options="templates"
-      label="Select a template"
-      @input="fetchTemplateDetails"
-    />
+    <template v-if="props.mode === 'new'">
+      <q-select
+        filled
+        v-model="selectedTemplate"
+        :options="templates"
+        label="Select a template"
+        @input="fetchTemplateDetails"
+        option-label="name"
+        option-value="id"
+      />
+    </template>
 
     <div class="q-pa-md">
       <q-btn
@@ -42,7 +46,7 @@
           <q-editor
             filled
             v-model="comment"
-            label="comment"
+            label="Comment"
             :dense="$q.screen.lt.md"
           />
         </q-card-section>
@@ -52,23 +56,9 @@
         </q-card-actions>
       </q-card>
     </q-dialog>
-
     <q-splitter v-model="splitterModel" style="height: 600px">
       <template v-slot:before>
         <div class="q-pa-md">
-          <q-btn
-            label="Create Group"
-            color="primary"
-            @click="showCreateGroupDialog = true"
-            class="q-ma-md"
-          />
-
-          <q-btn
-            label="Create Question"
-            color="secondary"
-            @click="showCreateQuestionDialog = true"
-            class="q-ma-md"
-          />
           <q-tree
             :nodes="groups"
             node-key="id"
@@ -77,18 +67,11 @@
             v-model:ticked="ticked"
             tick-strategy="leaf"
             ticked-color="primary"
-            @dblclick="editSelected"
           />
         </div>
       </template>
-
       <template v-slot:after>
-        <q-tab-panels
-          v-model="selectedNodeId"
-          animated
-          transition-prev="jump-up"
-          transition-next="jump-up"
-        >
+        <q-tab-panels v-model="selectedNodeId" animated>
           <q-tab-panel
             v-for="node in flattenedNodes"
             :key="node.id"
@@ -100,101 +83,6 @@
         </q-tab-panels>
       </template>
     </q-splitter>
-
-    <q-dialog v-model="showEditGroupDialog" persistent>
-      <q-card>
-        <q-card-section class="row items-center">
-          <div class="text-h6">Edit Group Name</div>
-        </q-card-section>
-        <q-card-section>
-          <q-select
-            filled
-            v-model="selectedGroupToEdit"
-            :options="groupOptions"
-            label="Select a group"
-          />
-          <q-input filled v-model="newGroupName" label="New Group Name" />
-        </q-card-section>
-        <q-card-actions align="right">
-          <q-btn flat label="Cancel" color="primary" v-close-popup />
-          <q-btn flat label="Save" color="primary" @click="editGroup" />
-        </q-card-actions>
-      </q-card>
-    </q-dialog>
-
-    <q-dialog v-model="showEditQuestionDialog" persistent>
-      <q-card>
-        <q-card-section class="row items-center">
-          <div class="text-h6">Edit Question</div>
-        </q-card-section>
-        <q-card-section>
-          <q-select
-            filled
-            v-model="selectedQuestionToEdit"
-            :options="questionOptions"
-            label="Select a question"
-          />
-          <q-input
-            filled
-            v-model="newQuestionTitle"
-            label="New Question Title"
-          />
-          <q-editor
-            filled
-            v-model="newQuestionDescription"
-            label="New Question Description"
-            class="q-mb-md"
-            :dense="$q.screen.lt.md"
-          />
-        </q-card-section>
-        <q-card-actions align="right">
-          <q-btn flat label="Cancel" color="primary" v-close-popup />
-          <q-btn flat label="Save" color="primary" @click="editQuestion" />
-        </q-card-actions>
-      </q-card>
-    </q-dialog>
-
-    <q-dialog v-model="showCreateGroupDialog" persistent>
-      <q-card>
-        <q-card-section class="row items-center">
-          <div class="text-h6">Create a new group</div>
-        </q-card-section>
-        <q-card-section>
-          <q-input filled v-model="groupName" label="Group Name" />
-        </q-card-section>
-        <q-card-actions align="right">
-          <q-btn flat label="Cancel" color="primary" v-close-popup />
-          <q-btn flat label="Create" color="primary" @click="addGroup" />
-        </q-card-actions>
-      </q-card>
-    </q-dialog>
-
-    <q-dialog v-model="showCreateQuestionDialog" persistent>
-      <q-card>
-        <q-card-section class="row items-center">
-          <div class="text-h6">Create a new question</div>
-        </q-card-section>
-        <q-card-section>
-          <q-input filled v-model="questionTitle" label="Question Title" />
-          <q-select
-            filled
-            v-model="selectedGroup"
-            :options="groupOptions"
-            label="Select a group"
-          />
-          <q-editor
-            filled
-            v-model="questionDescription"
-            label="Question Description"
-            :dense="$q.screen.lt.md"
-          />
-        </q-card-section>
-        <q-card-actions align="right">
-          <q-btn flat label="Cancel" color="primary" v-close-popup />
-          <q-btn flat label="Create" color="primary" @click="addQuestion" />
-        </q-card-actions>
-      </q-card>
-    </q-dialog>
   </div>
 </template>
 
@@ -202,7 +90,7 @@
 import { ref, computed, watch } from "vue";
 import { useAppStore } from "../../stores/appStore";
 import { useRouter } from "vue-router";
-import { v4 } from "uuid";
+import { v4 as uuidv4 } from "uuid";
 
 export default {
   props: {
@@ -218,13 +106,16 @@ export default {
   setup(props) {
     const splitterModel = ref(20);
     const store = useAppStore();
-    const templates = ref([]);
-    const selectedTemplate = ref(null);
-
-    const router = useRouter();
-    const projectName = ref("");
-    const company = ref("");
+    const comment = ref("");
+    const showCommentDialog = ref(false);
     const selected = ref("");
+    const saveComment = () => {
+      // Save your comment here
+      console.log(comment.value);
+
+      // Close the dialog
+      showCommentDialog.value = false;
+    };
     const ticked = ref("");
     const selectedNodeId = computed(() => {
       const node = flattenedNodes.value.find(
@@ -233,275 +124,113 @@ export default {
       return node ? node.id : null;
     });
 
-    const comment = ref("");
-    const saveComment = () => {
-      // Save your comment here
-      console.log(comment.value);
-
-      // Close the dialog
-      showCommentDialog.value = false;
-    };
-
-    const editSelected = () => {
-      const group = groups.value.find((g) => g.id === selected.value);
-      if (group) {
-        // The selected node is a group
-        selectedGroupToEdit.value = {
-          label: group.label,
-          value: group.id,
-        };
-        showEditGroupDialog.value = true;
-      } else {
-        // The selected node is a question
-        const question = flattenedNodes.value.find(
-          (n) => n.id === selected.value
-        );
-        if (question) {
-          selectedQuestionToEdit.value = {
-            label: question.label,
-            value: question.id,
-          };
-          showEditQuestionDialog.value = true;
-        }
-      }
-    };
-
-    const computedGroups = computed(() => {
-      return groups.value.map((group) => ({
-        ...group,
-        label: `${group.label} (${group.children.length})`,
-      }));
-    });
-
+    const templates = ref([]);
+    const selectedTemplate = ref(null);
     const groups = ref([]);
-
-    const showCreateGroupDialog = ref(false);
-    const showCreateQuestionDialog = ref(false);
-    const showCommentDialog = ref(false);
-
-    const groupName = ref("");
-
-    const showEditGroupDialog = ref(false);
-    const selectedGroupToEdit = ref("");
-    const newGroupName = ref("");
-    const showEditQuestionDialog = ref(false);
-    const selectedQuestionToEdit = ref("");
-    const newQuestionTitle = ref("");
-    const newQuestionDescription = ref("");
-
-    const editQuestion = () => {
-      if (
-        selectedQuestionToEdit.value.label.trim() === "" ||
-        newQuestionTitle.value.trim() === "" ||
-        newQuestionDescription.value.trim() === ""
-      ) {
-        return;
-      }
-
-      const group = groups.value.find((g) =>
-        g.children.find((c) => c.label === selectedQuestionToEdit.value.label)
-      );
-
-      if (group) {
-        const question = group.children.find(
-          (c) => c.label === selectedQuestionToEdit.value.label
-        );
-
-        if (question) {
-          question.label = newQuestionTitle.value;
-          question.description = newQuestionDescription.value;
-
-          // Sort the questions alphabetically within the group.
-          group.children.sort((a, b) => a.label.localeCompare(b.label));
-
-          newQuestionTitle.value = "";
-          newQuestionDescription.value = "";
-          selectedQuestionToEdit.value = "";
-          showEditQuestionDialog.value = false;
-        }
-      }
-    };
-
-    const questionOptions = computed(() => {
-      const options = [];
-      groups.value.forEach((group) => {
-        group.children.forEach((child) => {
-          options.push(child.label);
-        });
-      });
-      return options;
-    });
-    const editGroup = () => {
-      if (
-        selectedGroupToEdit.value.label.trim() === "" ||
-        newGroupName.value.trim() === ""
-      ) {
-        return;
-      }
-
-      const group = groups.value.find(
-        (g) => g.label === selectedGroupToEdit.value.label
-      );
-
-      if (group) {
-        group.label = newGroupName.value;
-
-        newGroupName.value = "";
-        selectedGroupToEdit.value = "";
-        showEditGroupDialog.value = false;
-
-        // Sort the groups alphabetically.
-        groups.value.sort((a, b) => a.label.localeCompare(b.label));
-      }
-    };
-
-    const addGroup = () => {
-      if (groupName.value.trim() === "") {
-        return;
-      }
-
-      groups.value = [
-        ...groups.value,
-        {
-          id: v4(), // Assign a UUID to the new group
-          label: groupName.value,
-          children: [],
-        },
-      ];
-
-      // Sort the groups alphabetically.
-      groups.value.sort((a, b) => a.label.localeCompare(b.label));
-
-      selected.value = groupName.value;
-      groupName.value = "";
-      showCreateGroupDialog.value = false;
-    };
-
-    const questionTitle = ref("");
-    const selectedGroup = ref("");
-    const questionDescription = ref("");
-
-    const addQuestion = () => {
-      if (
-        questionTitle.value.trim() === "" ||
-        questionDescription.value.trim() === "" ||
-        selectedGroup.value.trim() === ""
-      ) {
-        return;
-      }
-
-      const group = groups.value.find((g) => g.label === selectedGroup.value);
-
-      if (group) {
-        group.children = [
-          ...group.children,
-          {
-            id: v4(), // Assign a UUID to the new question
-            label: questionTitle.value,
-            description: questionDescription.value,
-            ticked: false,
-          },
-        ];
-
-        // Sort the questions alphabetically within the group.
-        group.children.sort((a, b) => a.label.localeCompare(b.label));
-
-        questionTitle.value = "";
-        questionDescription.value = "";
-        selectedGroup.value = "";
-        showCreateQuestionDialog.value = false;
-      }
-    };
     const flattenedNodes = computed(() => {
-      const nodes = [];
-      const traverse = (node) => {
-        if (node.children) {
-          node.children.forEach(traverse);
-        }
-        nodes.push(node);
-      };
-      groups.value.forEach(traverse);
-      return nodes;
+      return flattenNodes(groups.value);
     });
 
-    const groupOptions = computed(() => {
-      return groups.value.map((group) => group.label);
-    });
+    const router = useRouter();
+    const projectName = ref("");
+    const company = ref("");
 
-    if (props.mode === "new") {
-      store.fetchTemplates().then((fetchedTemplates) => {
-        if (Array.isArray(fetchedTemplates)) {
-          templates.value = fetchedTemplates.map((template) => ({
-            label: template.name,
-            value: template.id,
+    // Fetch templates and their details
+    const fetchTemplates = async () => {
+      if (props.mode === "new") {
+        store.installActions([
+          {
+            label: "CREATE PROJECT",
+            callback: () => {
+              router.back();
+            },
+          },
+        ]);
+
+        templates.value = await store.fetchTemplates();
+      } else if (props.mode === "edit") {
+        store.installActions([
+          {
+            label: "Save",
+            callback: () => {
+              router.back();
+            },
+          },
+          {
+            label: "CLONE",
+            callback: () => {
+              router.back();
+            },
+          },
+        ]);
+      }
+    };
+
+    // Fetch template groups and questions
+    const fetchTemplateDetails = async () => {
+      const templateId = selectedTemplate.value.id;
+
+      // Fetch template groups
+      const fetchedGroups = await store.fetchTemplateGroups(templateId);
+      groups.value = fetchedGroups.map((group) => ({
+        id: group.id,
+        label: group.groupName,
+        children: [],
+      }));
+
+      // Fetch template questions
+      for (const group of fetchedGroups) {
+        const groupId = group.id;
+        const fetchedQuestions = await store.fetchTemplateQuestions(groupId);
+        const targetGroup = groups.value.find((g) => g.id === groupId);
+        if (targetGroup) {
+          targetGroup.children = fetchedQuestions.map((question) => ({
+            id: question.id,
+            label: question.questionTitle,
+            description: question.questionDescription, // Add this line
           }));
-        } else {
-          // handle the case when fetchedTemplates is not an array
-          console.error("fetchedTemplates is not an array:", fetchedTemplates);
         }
-      });
+      }
+    };
 
-      store.installActions([
-        {
-          label: "CREATE PROJECT",
-          callback: () => {
-            router.back();
-          },
-        },
-      ]);
-    } else if (props.mode === "edit") {
-      store.installActions([
-        {
-          label: "Save",
-          callback: () => {
-            // Create a deep copy of the groups
+    // Function to flatten the groups and their questions for q-tree
+    const flattenNodes = (nodes) => {
+      const flattened = [];
+      for (const node of nodes) {
+        flattened.push(node);
+        if (node.children) {
+          flattened.push(...flattenNodes(node.children));
+        }
+      }
+      return flattened;
+    };
 
-            router.back();
-          },
-        },
-        {
-          label: "CLONE",
-          callback: () => {
-            router.back();
-          },
-        },
-      ]);
-    }
+    // Watch for changes in the selected template
+    watch(selectedTemplate, (newValue) => {
+      if (newValue) {
+        // Fetch template details
+        fetchTemplateDetails();
+      }
+    });
+
+    // Fetch templates on component mount
+    fetchTemplates();
 
     return {
       splitterModel,
+      templates,
       selectedNodeId,
-      questionTitle,
-      selectedGroup,
-      selected,
-      groups: computedGroups,
-      showCreateGroupDialog,
-      showCreateQuestionDialog,
-      showCommentDialog,
-      saveComment,
-      groupName,
-      addGroup,
-      addQuestion,
-      flattenedNodes,
-      groupOptions,
-      questionDescription,
-      showEditGroupDialog,
-      selectedGroupToEdit,
-      newGroupName,
-      editGroup,
-      showEditQuestionDialog,
-      selectedQuestionToEdit,
-      newQuestionTitle,
-      newQuestionDescription,
-      ticked,
-      editQuestion,
-      questionOptions,
-      editSelected,
+      selectedTemplate,
       projectName,
       company,
+      groups,
+      flattenedNodes,
+      props,
       comment,
-      templates,
-      selectedTemplate,
+      saveComment,
+      showCommentDialog,
+
+      selected,
     };
   },
 };
